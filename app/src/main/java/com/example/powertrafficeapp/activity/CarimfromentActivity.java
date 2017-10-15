bdcdcdfb94922d3afffbb7947aedb3812b74a05d
赵洪斌 <3185717649@qq.com>
2017-10-15 14:51:57 +0800

    csczxczxc

--- app/src/main/java/com/example/powertrafficeapp/activity/CarimfromentActivity.java
+++ app/src/main/java/com/example/powertrafficeapp/activity/CarimfromentActivity.java
@@ -15,6 +15,7 @@ import com.example.powertrafficeapp.fragment.carinfor.Carinfor_3;
 
 public class CarimfromentActivity extends AppCompatActivity implements RadioGroup.OnCheckedChangeListener {
     String hh = "";
+    String hsadasdh = "";
     private RadioGroup RadiogroupF8;
     private LinearLayout LinearF8Xin;
     private TextView text_name;
